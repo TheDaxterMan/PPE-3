6916bd0e99841a06365549277036a7b601fa8bf6
TheDaxterMan <maximecombeau24120@gmail.com>
2018-11-14 10:37:57 +0100

    include du menu en test

---
+++ friend-finder/menu.php
@@ -0,0 +1,55 @@
+<header id="header">
+  <nav class="navbar navbar-default navbar-fixed-top menu">
+    <div class="container">
+
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index-register.html"><img src="images/logo.png" alt="logo" /></a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right main-menu">
+          <li class="dropdown"><a href="index.html">Accueil</a></li>
+
+          <li class="dropdown"><a href="newsfeed.html">Fil d'actualité</a></li>
+
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profil <span><img src="images/down-arrow.png" alt="" /></span></a>
+            <ul class="dropdown-menu login">
+              <li><a href="timeline.html">Profil</a></li>
+              <li><a href="timeline-about.html">A propos de moi</a></li>
+              <li><a href="timeline-friends.html">Amis</a></li>
+              <li><a href="edit-profile-basic.html">Modifier les Informations</a></li>
+              <li><a href="edit-profile-work-edu.html">Modifier son Travail</a></li>
+              <li><a href="edit-profile-interests.html">Modifier les centres d'intérêts</a></li>
+              <li><a href="edit-profile-settings.html">Paramètre du compte</a></li>
+              <li><a href="edit-profile-password.html">Changer de Mot de passe</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle pages" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Toutes les Pages <span><img src="images/down-arrow.png" alt="" /></span></a>
+            <ul class="dropdown-menu page-list">
+              <li><a href="contact.html">Contact Us</a></li>
+              <li><a href="faq.html">FAQ Page</a></li>
+              <li><a href="404.html">404 Not Found</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="contact.html">Contact</a></li>
+        </ul>
+        <form class="navbar-form navbar-right hidden-sm">
+          <div class="form-group">
+            <i class="icon ion-android-search"></i>
+            <input type="text" class="form-control" placeholder="Rechercher des amis">
+          </div>
+        </form>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container -->
+  </nav>
+</header>
