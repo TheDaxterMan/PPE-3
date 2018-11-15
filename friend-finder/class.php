b7c311f1f1e4097198f04054488e26a91b4f215a
Akulaz <44777455+Akulaz@users.noreply.github.com>
2018-11-15 09:16:16 +0100

    Add files via upload
    Fichier class vide

---
+++ friend-finder/class.php
@@ -0,0 +1,38 @@
+<?php
+
+class
+	{
+		Private ;
+		Private	;
+		Private ;
+		Private ;
+
+			Public function  (, , , )
+			{
+				$this->  = ;
+				$this->  = ;
+				$this->  = ;
+				$this->  = ;
+			}
+
+			Public function ()
+			{
+				return $this-> ;
+			}
+
+			Public function ()
+			{
+				return $this-> ;
+			}
+
+			Public function ()
+			{
+				return $this-> ;
+			}
+
+			Public function ()
+			{
+				return $this-> ;
+			}
+
+?>
