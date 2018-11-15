42988f910f54290ca5f7c7ea0b099deb8f2a3c6b
TheDaxterMan <maximecombeau24120@gmail.com>
2018-11-15 09:19:52 +0100

    save

--- friend-finder/enregistrer.php
+++ friend-finder/enregistrer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $prenom=$_POST['prenom'];
 $nom=$_POST['nom'];
@@ -7,8 +6,3 @@ $ident=$_POST['ident'];
 $mdp=$_POST['mdp'];
 $mdp2=$_POST['mdp2'];
 ?>
-=======
-<?php
-
-?>
->>>>>>> b7c311f1f1e4097198f04054488e26a91b4f215a
