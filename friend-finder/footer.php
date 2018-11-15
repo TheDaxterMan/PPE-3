fdf46e31306d1e09144be75dd87bd9678881441d
TheDaxterMan <maximecombeau24120@gmail.com>
2018-11-15 15:00:49 +0100

    footer

--- friend-finder/footer.php
+++ friend-finder/footer.php
@@ -4,52 +4,16 @@
       <div class="footer-wrapper">
         <div class="col-md-3 col-sm-3">
           <a href="#"><img src="images/logo.png" alt="" class="footer-logo" /></a>
-          <ul class="list-inline social-icons">
-            <li><a href="#"><i class="icon ion-social-facebook"></i></a></li>
-            <li><a href="#"><i class="icon ion-social-twitter"></i></a></li>
-            <li><a href="#"><i class="icon ion-social-googleplus"></i></a></li>
-            <li><a href="#"><i class="icon ion-social-pinterest"></i></a></li>
-            <li><a href="#"><i class="icon ion-social-linkedin"></i></a></li>
-          </ul>
         </div>
-        <div class="col-md-2 col-sm-2">
-          <h5>For individuals</h5>
-          <ul class="footer-links">
-            <li><a href="#">Signup</a></li>
-            <li><a href="#">login</a></li>
-            <li><a href="#">Explore</a></li>
-            <li><a href="#">Finder app</a></li>
-            <li><a href="#">Features</a></li>
-            <li><a href="#">Language settings</a></li>
-          </ul>
-        </div>
-        <div class="col-md-2 col-sm-2">
-          <h5>For businesses</h5>
-          <ul class="footer-links">
-            <li><a href="#">Business signup</a></li>
-            <li><a href="#">Business login</a></li>
-            <li><a href="#">Benefits</a></li>
-            <li><a href="#">Resources</a></li>
-            <li><a href="#">Advertise</a></li>
-            <li><a href="#">Setup</a></li>
-          </ul>
-        </div>
-        <div class="col-md-2 col-sm-2">
-          <h5>About</h5>
-          <ul class="footer-links">
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Contact us</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Terms</a></li>
-            <li><a href="#">Help</a></li>
-          </ul>
+        <div class="">
+
         </div>
         <div class="col-md-3 col-sm-3">
           <h5>Contactez nous</h5>
           <ul class="contact">
             <li><i class="icon ion-ios-telephone-outline"></i>0800 820 075</li>
-            <li><i class="icon ion-ios-email-outline"></i>info@stannah.fr</li>
-            <li><i class="icon ion-ios-location-outline"></i>Dans ton escalier</li>
+            <li><i class="icon ion-ios-email-outline"></i>secretariat@bahuet.org</li>
+            <li><i class="icon ion-ios-location-outline"></i>41 bis avenue Edmond Michelet 19100 BRIVE-La-Gaillarde</center></li>
           </ul>
         </div>
       </div>
