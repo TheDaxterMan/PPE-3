d9b95a0adcaea13971544749b48a2bf382178cec
Bayja <virlouvetmaxime9@gmail.com>
2018-11-21 09:44:29 +0100

    Création page login php

---
+++ friend-finder/login.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+  <title>Connexion</title>
+</head>
+<body>
+<?php
+
+  //On fais la connexion à la base de données\\
+  if (isset ($_POST['submit']))
+    $sbh=new mysqli ("localhost", "root", "", "ppe");
+    //recupération des données de la base //
+    $nom=$_POST['pseudo'];
+    $mail=$_POST['mail'];
+    $mdp=$_POST['mdp'];
+  //requete sql qui permet d'enregistré dans la base de données//
+  $requete=$dbh->query("SELECT id,pseudo,mdp FROM partie_client
+                        WHERE pseudo='$nom'
+                        AND mdp='$mdp';");
+//On vérifie que le si les conditions sont respectés//
+  $result=$requete->fetch();
+    if(!$result){
+      echo "Votre pseudo ou votre mot de passe est incorrect.";
+      header('Refresh: 5; URL=../login.html'); // rafraîchit la page au bout 4 de secondes //
+  }
+    else{
+      // si le pseudo/mdp correct //
+      session_start(); // on démarre la session //
+      $_SESSION['id']=$result['id']; // on vérifie que l'id de la base est équivalent à l'id qu'on à reçu //
+      $_SESSION['pseudo']=$result['pseudo']; // on vérifie que le pseudo de la base est équivalent à l'id qu'on à reçu //
+      echo "Pseudo : ",$_SESSION['pseudo'],"<br> ID : ",$_SESSION['id'];
+      echo "<br><br> Vous êtes connecté.";
+      echo "<br> Vous allez être redigé vers la page d'acceuil.";
+      header('Refresh: 4; URL=../Site_Cybernet.html');
+  }
+?>
