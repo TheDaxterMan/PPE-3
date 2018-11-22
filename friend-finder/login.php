73cb0bef619d32ea30a930c0007d9c772e23eddf
Bayja <virlouvetmaxime9@gmail.com>
2018-11-22 09:08:14 +0100

    login fonctionnel

--- friend-finder/login.php
+++ friend-finder/login.php
@@ -4,7 +4,10 @@
 </head>
 <body>
 <?php
-$user = $_POST['user'];
+
+include "bdd.inc.php";
+
+$user = $_POST['login'];
 $mdp = $_POST['mdp'];
 $sql="SELECT * FROM utilisateur
           WHERE login_utilisateur = '$user'
@@ -13,11 +16,14 @@ $sql="SELECT * FROM utilisateur
 $req = $conn -> prepare($sql)or die($conn->errorInfo());
 $req -> execute();
 $res=$req->fetch();
-if ($res['n_matricule'] == $user || $res['mdp'] == $mdp)
+if ($res['login_utilisateur'] == $user || $res['mdp_utilisateur'] == $mdp)
 {
 // crée le cookie avec le nom d'utilisateur et la session
 session_start();
-$_SESSION['id_user'] = $res['id_user']; // cette ligne crée une variable de session, où l'on sauve l'id de notre utilisateur connecté
-$_SESSION['permission'] = $res['id_grp']; // pareil pour ses permissions
-$_SESSION['matricule'] = $res['n_matricule'];
+$_SESSION['id_utilisateur'] = $res['id_utilisateur']; // cette ligne crée une variable de session, où l'on sauve l'id de notre utilisateur connecté
+//$_SESSION['login_utilisateur'] = $res['login_utilisateur'];
+}
+else {
+  echo "ça marche pas";
+}
 ?>
