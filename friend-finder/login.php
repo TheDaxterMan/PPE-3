6f71d36dcc6d9a43bac4aa1834d215d6ea95f52f
TheDaxterMan <maximecombeau24120@gmail.com>
2018-11-22 09:16:01 +0100

    mise en page

--- friend-finder/login.php
+++ friend-finder/login.php
@@ -5,25 +5,28 @@
 <body>
 <?php
 
-include "bdd.inc.php";
+  include "bdd.inc.php";
 
-$user = $_POST['login'];
-$mdp = $_POST['mdp'];
-$sql="SELECT * FROM utilisateur
-          WHERE login_utilisateur = '$user'
-          AND mdp_utilisateur = '$mdp'";
+  $user = $_POST['login'];
+  $mdp = $_POST['mdp'];
+  $sql="SELECT * FROM utilisateur
+            WHERE login_utilisateur = '$user'
+            AND mdp_utilisateur = '$mdp'";
 
-$req = $conn -> prepare($sql)or die($conn->errorInfo());
-$req -> execute();
-$res=$req->fetch();
-if ($res['login_utilisateur'] == $user || $res['mdp_utilisateur'] == $mdp)
-{
-// crée le cookie avec le nom d'utilisateur et la session
-session_start();
-$_SESSION['id_utilisateur'] = $res['id_utilisateur']; // cette ligne crée une variable de session, où l'on sauve l'id de notre utilisateur connecté
-//$_SESSION['login_utilisateur'] = $res['login_utilisateur'];
-}
-else {
-  echo "ça marche pas";
-}
+  $req = $conn -> prepare($sql)or die($conn->errorInfo());
+  $req -> execute();
+  $res=$req->fetch();
+  if ($res['login_utilisateur'] == $user || $res['mdp_utilisateur'] == $mdp)
+  {
+    // crée le cookie avec le nom d'utilisateur et la session
+    session_start();
+    $_SESSION['id_utilisateur'] = $res['id_utilisateur']; // cette ligne crée une variable de session, où l'on sauve l'id de notre utilisateur connecté
+    //$_SESSION['login_utilisateur'] = $res['login_utilisateur'];
+    header('Location: ./newsfeed.php');
+    exit();
+  }
+  else
+  {
+    echo "ça marche pas";
+  }
 ?>
