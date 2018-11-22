cce023dcc5e865a1b70dbf0a1d903fded57cd4c7
TheDaxterMan <maximecombeau24120@gmail.com>
2018-11-22 09:06:10 +0100

    login.inc

--- friend-finder/login.inc.php
+++ friend-finder/login.inc.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
 if (!isset($_SESSION['id_utilisateur']))
-=======
-if (!isset($_SESSION['id_user']))
->>>>>>> cfbd22907a596d35a7f50578988bbf5b8301c7a9
 {
   header('Location: ./index.php');
   exit();
