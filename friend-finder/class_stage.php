ce9308be5bb75df7a8085642b87a552935f58472
Akulaz <44777455+Akulaz@users.noreply.github.com>
2019-03-12 17:19:09 +0100

    Update class_stage.php

--- friend-finder/class_stage.php
+++ friend-finder/class_stage.php
@@ -15,22 +15,24 @@ class stage
 		Private $id_stage;
 		Private $lib_stage;
 		Private $desc_stage;
-    Private $dated_stage;
+    		Private $dated_stage;
 		Private $datef_stage;
 		Private $commentaire_stage;
+		Private $etat_stage;
 
 		/* ---------------------- */
 		/* class Stage Constructeur */
 		/* ---------------------- */
 
-			Public function stage ( $id_sta, $lib_sta, $desc_sta, $dated_sta, $datef_sta, $comm_sta)
+			Public function stage ( $id_sta, $lib_sta, $desc_sta, $dated_sta, $datef_sta, $comm_sta, $etat_sta)
 			{
 				$this -> id_stage = $id_sta;
 				$this -> lib_stage = $lib_sta;
 				$this -> desc_stage = $desc_sta;
-        $this -> dated_stage = $dated_sta;
+       	 			$this -> dated_stage = $dated_sta;
 				$this -> datef_stage = $datef_sta;
 				$this -> commentaire_stage = $comm_sta;
+				$this -> etat_stage = $etat_sta;
 
 			}
 
@@ -43,9 +45,10 @@ class stage
 				$data = $this->$id_sta;
 				$data = $data.$this->$lib_sta;
 				$data = $data.$this->$desc_sta;
-        $data = $data.$this->$dated_sta;
+        			$data = $data.$this->$dated_sta;
 				$data = $dataf.$this->$date_sta;
 				$data = $data.$this->$comm_sta;
+				$data = $data.$this->$etat_sta;
 				return $data;
 			}
 
@@ -82,6 +85,11 @@ class stage
 			{
 				return $this-> commentaire_stage;
 			}
+	
+			Public function  get_etat_stage ()
+			{
+				return $this-> etat_stage;
+			}
 
 			/* ---------------------- */
 			/* class Stage SET */
@@ -102,14 +110,20 @@ class stage
 				 $this-> dated_stage = $dated_sta;
 			}
 
-      Public function set_datef_stage ($datef_sta)
-      {
-         $this-> datef_stage = $datef_sta;
-      }
+      			Public function set_datef_stage ($datef_sta)
+      			{
+        			 $this-> datef_stage = $datef_sta;
+      			}
 
 			Public function set_commentaire_stage ($comm_sta)
 			{
 				 $this-> commentaire_stage = $comm_sta;
+			}
+	
+	
+			Public function set_etat_stage ($etat_sta)
+			{
+				 $this-> etat_stage = $etat_sta;
 			}
       /* ---------------------- */
       /* class Stage fonctions publiques */
@@ -119,11 +133,12 @@ class stage
 					$id_sta = $objet->get_id_stage();
 					$lib_sta = $objet->get_lib_stage();
 					$desc_sta = $objet->get_desc_stage();
-          $dated_sta = $objet->get_dated_stage();
+          				$dated_sta = $objet->get_dated_stage();
 					$datef_sta = $objet->get_datef_stage();
 					$comm_sta = $objet->get_commentaire_stage();
-
-					print $SQL = " INSERT INTO stage values (NULL, '$lib_sta', '$desc_sta', '$dated_sta', '$datef_sta', '$comm_sta', '1', '1', '1')";
+					$etat_sta = $objet->get_etat_stage();
+	      
+					print $SQL = " INSERT INTO stage values (NULL, '$lib_sta', '$desc_sta', '$dated_sta', '$datef_sta', '$comm_sta', '1', '1', '1', '0')";
 					$Req = $conn -> query ($SQL) or die (' Erreur ajout stage ');
 				}
 
