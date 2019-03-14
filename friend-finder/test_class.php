821e6ccbea6301aba5db6ebda5827747135f29ce
Akulaz <44777455+Akulaz@users.noreply.github.com>
2019-03-14 15:11:00 +0100

    Update test_class.php

--- friend-finder/test_class.php
+++ friend-finder/test_class.php
@@ -1,9 +1,29 @@
 <?php
+
+
 include 'class_utilisateur.php';
 include 'bdd.inc.php';
-include 'class_entreprise.php';
 
-/*$unutilisateur = new utilisateur (NULL, 'nomutil', 'prenomutil',  054561546540, 'mailf@zefaaezza.com', 'rueutil', 'villeutil', 615464, 'photoutil', 'loginutil',  1235456789);
+echo "Bonjour";
+/*
+	  $unutilisateur = new utilisateur (NULL, 'nomufrzfetil', 'prenomurftil',  0545544561546540, 'mairfefelf@zefaaezza.com', 'rueutferfil', 'villerfreeeutil', 6154489464, 'photofrerfutil', 'loginzerezutil', 'mdpfzef',0 ,'');
+
+      $unutilisateur -> ajout_utilisateur($unutilisateur, $conn);
+
+      var_dump($unutilisateur);
+
+	  echo"test fait ! ";*/
+	  
+	  $unutilisateur = new utilisateur('','','','','','','','','','','','','');
+	  
+	 $unutilisateur ->suppr_fictive($unutilisateur, $conn);
+	  
+	  var_dump($unutilisateur);
+	  
+	  
+/*include 'class_entreprise.php';
+
+$unutilisateur = new utilisateur (NULL, 'nomutil', 'prenomutil',  054561546540, 'mailf@zefaaezza.com', 'rueutil', 'villeutil', 615464, 'photoutil', 'loginutil',  1235456789);
 $uneentreprise = new entreprise (NULL, 'noment', 'nomresp',  'prenomresp', 'mail@zefaaezza.com', 'rueutil', 'villeent',  6154654, 'photoent', 'loginent',  12354456789, 1);
 
 var_dump($unutilisateur->getallutil());
