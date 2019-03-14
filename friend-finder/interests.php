08436138d7ce5a4fce01f735d3919f473857eba3
TheDaxterMan <maximecombeau24120@gmail.com>
2019-03-14 11:44:04 +0100

    msg erreur interet

--- friend-finder/interests.php
+++ friend-finder/interests.php
@@ -2,14 +2,23 @@
 session_start();
 include "bdd.inc.php";
 
+if ($_POST['interet']==NULL)
+{
+  echo"<script language=\"javascript\">";
+  echo"alert('PD')";
+  echo"</script>";
+  header('Refresh: 0; URL=./edit-profile-interests.php');
+}
+else
+{
+  $id_int=$_POST['interet'];
+  $id_utilisateur=$_SESSION['id'];
+  $sql="INSERT INTO interesse
+        VALUES ('$id_int', '$id_utilisateur')";
 
-$id_int=$_POST['interet'];
-$id_utilisateur=$_SESSION['id'];
-$sql="INSERT INTO interesse
-      VALUES ('$id_int', '$id_utilisateur')";
+  $req = $conn -> query($sql)or die($conn->errorInfo());
+  $res = $req->fetch();
 
-$req = $conn -> query($sql)or die($conn->errorInfo());
-$res = $req->fetch();
-
-header('Refresh: 0; URL=./edit-profile-interests.php');
+  header('Refresh: 0; URL=./edit-profile-interests.php');
+}
 ?>
