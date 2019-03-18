a2ed63ff75963b9e411a9f20979c81f35fba592f
Akulaz <44777455+Akulaz@users.noreply.github.com>
2019-03-18 06:00:29 +0100

    Update ajout_modif_filiere.php

--- friend-finder/ajout_modif_filiere.php
+++ friend-finder/ajout_modif_filiere.php
@@ -9,10 +9,10 @@ if (isset($_POST['enregistrerf']))
 {
   $lib_filiere=$_POST['filiere'];
 
-  $sql="INSERT INTO filiere VALUES (NULL,'$lib_filiere','0')";
+  $unefiliere = new filiere (NULL,'$lib_filiere',0);
+  
+  $unefiliere -> ajout_filiere($unefiliere, $conn);
 
-  $req = $conn -> prepare($sql)or die($conn->errorInfo());
-  $req -> execute();
   header('Location: ./modif_filiere.php');
 }
 if (isset($_POST['modifierf']))
@@ -20,11 +20,10 @@ if (isset($_POST['modifierf']))
   $id_filiere=$_POST['id_filiere'];
   $lib_filiere=$_POST['lib_filiere'];
 
-  $sql="UPDATE filiere SET lib_filiere = '$lib_filiere'
-                                 WHERE id_filiere = $id_filiere";
-
-  $req = $conn -> prepare($sql)or die($conn->errorInfo());
-  $req -> execute();
+  $autrefiliere = ('','','');
+  
+  $autrefiliere -> modif_filiere($autrefiliere, $conn);
+  
   header('Location: ./modif_filiere.php');
 }
 ?>
