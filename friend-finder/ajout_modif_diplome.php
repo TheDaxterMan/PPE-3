69dd0fb5a00d374c4150b5e852bc67b6661bc950
Akulaz <44777455+Akulaz@users.noreply.github.com>
2019-03-18 05:42:07 +0100

    Update ajout_modif_diplome.php

--- friend-finder/ajout_modif_diplome.php
+++ friend-finder/ajout_modif_diplome.php
@@ -9,11 +9,11 @@ if (isset($_POST['enregistrerd']))
 {
   $nom_diplome=$_POST['diplome'];
   $lib_diplome=$_POST['lib'];
+  
+  $undiplome = new diplome (NULL,'$nom_diplome','$lib_diplome',0);
+  
+  $undiplome -> ajout_diplome($undiplome, $conn);
 
-  $sql="INSERT INTO diplome VALUES (NULL,'$nom_diplome','$lib_diplome','0')";
-
-  $req = $conn -> prepare($sql)or die($conn->errorInfo());
-  $req -> execute();
   header('Location: ./modif_diplome.php');
 }
 if (isset($_POST['modifierd']))
@@ -21,13 +21,11 @@ if (isset($_POST['modifierd']))
   $id_diplome=$_POST['id_diplome'];
   $nom_diplome=$_POST['nom_diplome'];
   $lib_diplome=$_POST['lib_diplome'];
+  
+  $autrediplome = ('','','','');
+  
+  $autrediplome -> modif_diplome($autrediplome, $conn);
 
-  $sql="UPDATE diplome SET nom_diplome = '$nom_diplome',
-                                lib_diplome = '$lib_diplome'
-                                 WHERE id_diplome = $id_diplome";
-
-  $req = $conn -> prepare($sql)or die($conn->errorInfo());
-  $req -> execute();
   header('Location: ./modif_diplome.php');
 }
 ?>
