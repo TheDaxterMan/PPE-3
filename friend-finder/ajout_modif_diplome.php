6efb1c0380ccdeae5c10c756abbc168b02ee8029
Akulaz <44777455+Akulaz@users.noreply.github.com>
2019-03-18 21:34:44 +0100

    Update ajout_modif_diplome.php

--- friend-finder/ajout_modif_diplome.php
+++ friend-finder/ajout_modif_diplome.php
@@ -18,13 +18,13 @@ if (isset($_POST['enregistrerd']))
 }
 if (isset($_POST['modifierd']))
 {
-  $id_diplome=$_POST['id_diplome'];
-  $nom_diplome=$_POST['nom_diplome'];
-  $lib_diplome=$_POST['lib_diplome'];
   
-  $autrediplome = new diplome ('','','','');
-  
-  $autrediplome -> modif_diplome($autrediplome, $conn);
+  $sql="UPDATE diplome SET nom_diplome = '$nom_diplome',
+                                lib_diplome = '$lib_diplome'
+                                 WHERE id_diplome = $id_diplome";
+
+  $req = $conn -> prepare($sql)or die($conn->errorInfo());
+  $req -> execute();
 
   header('Location: ./modif_diplome.php');
 }
