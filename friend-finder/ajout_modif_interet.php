7692b946a5de50cf22b5cb23d80203cbd70e7f1d
Akulaz <44777455+Akulaz@users.noreply.github.com>
2019-03-18 06:10:14 +0100

    Update ajout_modif_interet.php

--- friend-finder/ajout_modif_interet.php
+++ friend-finder/ajout_modif_interet.php
@@ -3,17 +3,17 @@ session_start();
 require_once('login.inc.php');
 include "bdd.inc.php";
 include "login.inc.php";
-
+include "class_centres_interets.php";
 
 if (isset($_POST['enregistreri']))
 {
   $lib_interet=$_POST['interet'];
   $img_interet=$_POST['img'];
 
-  $sql="INSERT INTO centres_interets VALUES (NULL,'$lib_interet','$img_interet','0')";
-
-  $req = $conn -> prepare($sql)or die($conn->errorInfo());
-  $req -> execute();
+  $uninteret = new interet (NULL,'$lib_interet','$img_interet',0);
+  
+  $uninteret -> ajout_interet($uninteret, $conn);
+  
   header('Location: ./modif_interet.php');
 }
 if (isset($_POST['modifieri']))
@@ -21,13 +21,11 @@ if (isset($_POST['modifieri']))
   $id_interet=$_POST['id_interet'];
   $lib_interet=$_POST['lib_interet'];
   $img_interet=$_POST['img'];
-
-  $sql="UPDATE centres_interets SET lib_interet = '$lib_interet',
-                                    img_interet = '$img_interet'
-                                 WHERE id_interet = $id_interet";
-
-  $req = $conn -> prepare($sql)or die($conn->errorInfo());
-  $req -> execute();
+  
+   $autreinteret = ('','','','');
+  
+  $autreinteret -> modif_interet($autreinteret, $conn);
+  
   header('Location: ./modif_interet.php');
 }
 ?>
