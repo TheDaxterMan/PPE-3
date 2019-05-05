1190e46ff04ad1b1561378769a16d147fab79ae2
Akulaz <44777455+Akulaz@users.noreply.github.com>
2019-05-05 15:16:20 +0200

    Update ajout_modif_interet.php

--- friend-finder/ajout_modif_interet.php
+++ friend-finder/ajout_modif_interet.php
@@ -10,7 +10,7 @@ if (isset($_POST['enregistrer']))
   $lib_interet=$_POST['interet'];
   $img_interet=$_POST['img'];
 
-  $uneinteret = new interet (NULL,'$lib_interet','$img_interet','0')
+  $uneinteret = new interet (NULL,'$lib_interet','$img_interet','0');
   $uneinteret -> ajout_interet($uneinteret, $conn);
   
   $req = $conn -> prepare($sql)or die($conn->errorInfo());
